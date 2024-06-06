96f8c7be00054d2d872ba20dda0fdda53f43bbd0
JensonMedina <medinajenson74@gmail.com>
2024-06-06 11:35:51 -0300

    Mi portfolio

---
+++ js/index.js
@@ -0,0 +1,16 @@
+let contador = 1;
+function eventoMenu(){
+    const menu = document.querySelector('#menu');
+    const btnMnu = document.querySelector('#btnMnu');
+	if(contador==1){
+		//menu.style='left: 0';
+        menu.style.transform = 'translateX(-0%)';
+		contador=0;
+        btnMnu.innerHTML='x';
+	}else{
+		//menu.style='left:-100%';
+        menu.style.transform = 'translateX(-120%)';
+		contador=1;
+        btnMnu.innerHTML='≡';
+	};
+};

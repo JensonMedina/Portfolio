03ceb38c56536ac42481832d6909c7bbb19f2000
JensonMedina <medinajenson74@gmail.com>
2024-06-11 18:46:27 -0300

    Realice la funcion de enviar email

--- js/index.js
+++ js/index.js
@@ -1,16 +1,39 @@
 let contador = 1;
-function eventoMenu(){
-    const menu = document.querySelector('#menu');
-    const btnMnu = document.querySelector('#btnMnu');
-	if(contador==1){
-		//menu.style='left: 0';
-        menu.style.transform = 'translateX(-0%)';
-		contador=0;
-        btnMnu.innerHTML='x';
-	}else{
-		//menu.style='left:-100%';
-        menu.style.transform = 'translateX(-120%)';
-		contador=1;
-        btnMnu.innerHTML='≡';
-	};
+function eventoMenu() {
+  const menu = document.querySelector("#menu");
+  const btnMnu = document.querySelector("#btnMnu");
+  if (contador == 1) {
+    //menu.style='left: 0';
+    menu.style.transform = "translateX(-0%)";
+    contador = 0;
+    btnMnu.innerHTML = "x";
+  } else {
+    //menu.style='left:-100%';
+    menu.style.transform = "translateX(-120%)";
+    contador = 1;
+    btnMnu.innerHTML = "≡";
+  }
+}
+
+window.onload = function () {
+  document
+    .getElementById("frmContacto")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      // these IDs from the previous steps
+      emailjs.sendForm("contact_service", "contact_form", this).then(
+        () => {
+          console.log("SUCCESS!");
+          clearForm();
+        },
+        (error) => {
+          console.log("FAILED...", error);
+        }
+      );
+    });
 };
+
+function clearForm() {
+  let formulario = document.getElementById("frmContacto");
+  formulario.reset();
+}
